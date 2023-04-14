aee4f11c1f680c9ecf3e2ffa647724744b14cf4e
Andres Osuna <andres.osuna@epicor.com>
2023-04-13 21:19:27 -0600

    Mejoras

--- src/app/pages/tables/abm-students/abm-students.component.ts
+++ src/app/pages/tables/abm-students/abm-students.component.ts
@@ -57,4 +57,12 @@ export class AbmStudentsComponent {
       this.studentsForm.markAllAsTouched();
     }
   }
+
+  numberOnly(event: any): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+  }
 }
